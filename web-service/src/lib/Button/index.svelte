d9efd11ddcdb9d7159d82d2a78b4c3dcec6a16e2
Den Kochetkov <d@zdp.su>
2021-08-10 12:45:58 +0300

    Hero mobile layout done

---
+++ web-service/src/lib/Button/index.svelte
@@ -0,0 +1,48 @@
+<script>
+  export let button;
+</script>
+
+{#if button.link && button.link !== ''}
+  <a href={button.link} class="{button.size} {button.color}" disabled={button.disabled}>
+    {button.text}
+  </a>
+{:else}
+  <button on:click|preventDefault>
+    {button.text}
+  </button>
+{/if}
+
+<style>
+  a,
+  button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 46px;
+    width: 100%;
+    color: #fff;
+    margin: 0;
+    padding: 0 15px;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 0px 4px 4px 3px rgba(38, 38, 38, 0.05);
+  }
+  .blue {
+    border: 1px solid #2C14B2;
+    background: #2C14B2;
+  }
+  .red {
+    border: 1px solid #E62B32;
+    background: #E62B32;
+  }
+  .big {
+    height: 70px;
+    font-size: 18px;
+    line-height: 21px;
+    padding: 0 30px;
+  }
+</style>
---
+++ web-service/src/lib/Hero/index.svelte
@@ -0,0 +1,78 @@
+<script>
+  import Button from '$lib/Button/index.svelte';
+import { bubble } from 'svelte/internal';
+
+  export let heroData;
+</script>
+
+<section style="color: {heroData.textColor};">
+  <img src={heroData.cover.file.url} alt={heroData.cover.alt}>
+  <div class="overlay" style="background: {heroData.overlayBg};"></div>
+  <div class="container">
+    <h1>
+      {heroData.header}
+    </h1>
+    <div class="content">
+      {@html heroData.content}
+    </div>
+    <div class="buttons">
+      {#each heroData.buttons as button}
+        {#if button.isActive === true}
+          <Button bind:button />
+        {/if}
+      {/each}
+    </div>
+  </div>
+</section>
+
+<style>
+  section {
+    z-index: 1;
+    position: relative;
+    margin-top: -110px;
+    padding-top: 170px;
+    padding-bottom: 60px;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -2;
+    object-fit: cover;
+    object-position: center;
+  }
+  .overlay {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: -1;
+    pointer-events: none;
+  }
+  .container {
+    z-index: 1;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+  }
+  h1 {
+    font-weight: 700;
+    font-size: 40px;
+    line-height: 47px;
+  }
+  .content {
+    display: none;
+    font-size: 20px;
+    line-height: 23px;
+    grid-template-columns: 1fr;
+    gap: 23px;
+  }
+  .buttons {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 20px;
+  }
+</style>
