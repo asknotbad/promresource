8254e03fcdd97b022090e89f1914724c8084c8e0
Den Kochetkov <d@zdp.su>
2021-08-20 02:00:19 +0300

    NewsList mobile layout done

---
+++ web-service/src/lib/NewsList/ListItem.svelte
@@ -0,0 +1,54 @@
+<script>
+  import arrow from './arrow.svg';
+
+  export let item;
+</script>
+
+{#if item}
+  <article>
+    <img src={item.cover.file.url} alt={item.cover.alt}>
+    <h3>
+      {item.header}
+    </h3>
+    <a href="/">
+      Подробнее
+      <img src={arrow} alt="">
+    </a>
+  </article>
+{/if}
+
+<style>
+  article {
+    border: 1px solid #363433;
+    padding-bottom: 27px;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 25px;
+  }
+  article > img {
+    width: 100%;
+    height: 236px;
+    object-fit: cover;
+    object-position: center;
+  }
+  h3 {
+    margin: 0 15px;
+  }
+  a {
+    margin: 0 15px;
+    display: grid;
+    grid-template-columns: min-content 32px;
+    justify-content: start;
+    align-items: center;
+    text-decoration: none;
+    gap: 10px;
+    margin-right: auto;
+    color: #E52B32;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+  }
+  a img {
+    width: 100%;
+  }
+</style>
---
+++ web-service/src/lib/NewsList/index.svelte
@@ -0,0 +1,44 @@
+<script>
+  import ListItem from "./ListItem.svelte";
+  export let newsListData;
+  export let news;
+  export let noPadding = false;
+  export let isLastOnPage = false;
+</script>
+
+{#if newsListData && news}
+  <section class:no-padding={noPadding} class:last={isLastOnPage}>
+    <div class="container">
+      <h2>
+        {newsListData.header}
+      </h2>
+      <div class="cards">
+        {#each news as item}
+          <ListItem bind:item />
+        {/each}
+      </div>
+    </div>
+  </section>
+{/if}
+
+<style>
+  section {
+    padding: 60px 0;
+  }
+  section.no-padding {
+    padding: 0;
+  }
+  section.last {
+    padding-bottom: 60px;
+  }
+  .container {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+  }
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 30px;
+  }
+</style>
