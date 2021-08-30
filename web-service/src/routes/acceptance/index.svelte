34cfb61fabafb09086cc77b0393bca925b7cb3b6
Den Kochetkov <d@zdp.su>
2021-08-30 12:07:27 +0300

    /acceptance route added

---
+++ web-service/src/routes/acceptance/index.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+		// const aboutUsTextDataRes = await fetch(`/api/about-us-text-data`);
+		// aboutUsTextData = await aboutUsTextDataRes.json();
+	});
+
+</script>
+
+<svelte:head>
+	<title>Промресурс | Приёмка лома</title>
+</svelte:head>
