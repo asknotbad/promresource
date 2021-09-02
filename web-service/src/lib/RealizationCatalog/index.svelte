5920dd21cca0f7f7eeb80110eaa6cf18c7f0f650
Den Kochetkov <d@zdp.su>
2021-09-02 16:18:33 +0300

    added localization to realizationCatalogData

---
+++ web-service/src/lib/RealizationCatalog/index.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let catalogData;
+  export let realizationCatalogData;
+</script>
+
+{#if catalogData && realizationCatalogData}
+  <section>
+    <div class="container">
+      RealizationCatalog
+    </div>
+  </section>
+{/if}
+
+<style>
+  section {
+    padding: 60px 0;
+  }
+</style>
--- web-service/src/routes/realization/index.svelte
+++ web-service/src/routes/realization/index.svelte
@@ -1,13 +1,21 @@
 <script>
   import { onMount } from 'svelte';
   import RealizationHero from '$lib/RealizationHero/index.svelte';
+  import RealizationCatalog from '$lib/RealizationCatalog/index.svelte';
 
   let realizationHeroData;
+  let catalogData;
+  let realizationCatalogData;
 
   onMount(async () => {
 		const realizationHeroDataRes = await fetch(`/api/realization-hero-data`);
 		realizationHeroData = await realizationHeroDataRes.json();
 
+    const catalogDataRes = await fetch(`/api/catalog?type=sell&isActive=true&_sort=order:ASC`);
+		catalogData = await catalogDataRes.json();
+
+    const realizationCatalogDataRes = await fetch(`/api/realization-catalog-data`);
+		realizationCatalogData = await realizationCatalogDataRes.json();
 	});
 </script>
 
@@ -17,4 +25,8 @@
 
 {#if realizationHeroData}
   <RealizationHero bind:realizationHeroData />
+{/if}
+
+{#if realizationCatalogData}
+  <RealizationCatalog bind:realizationCatalogData bind:catalogData />
 {/if}
