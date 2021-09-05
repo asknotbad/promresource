50e72dba433fe30ee1ac1ff9806994df1965458d
Den Kochetkov <d@zdp.su>
2021-09-05 22:01:08 +0300

    Sales mobile layout done

--- web-service/src/lib/Employees/Sales.svelte
+++ web-service/src/lib/Employees/Sales.svelte
@@ -1,11 +1,23 @@
 <script>
+  import Item from '$lib/Vacancies/Item.svelte';
+import SalesItemCard from './SalesItemCard.svelte';
+
   export let salesData;
 </script>
 
 {#if salesData}
   <section>
     <div class="container">
-      Sales
+      <h2>
+        {salesData.header}
+      </h2>
+      <div class="cards">
+        {#each salesData.items as item}
+          {#if item.isActive === true}
+            <SalesItemCard bind:item />
+          {/if}
+        {/each}
+      </div>
     </div>
   </section>
 {/if}
@@ -13,5 +25,13 @@
 <style>
   section {
     padding: 60px 0;
+  }
+  h2 {
+    margin-bottom: 30px;
+  }
+  .cards {
+    display: grid;
+    grid-template-columns: 1fr;
+    row-gap: 20px;
   }
 </style>
---
+++ web-service/src/lib/Employees/SalesItemCard.svelte
@@ -0,0 +1,64 @@
+<script>
+  export let item;
+</script>
+
+{#if item}
+  <article>
+    <div class="photo">
+      <img src={item.photo.file.url} alt={item.photo.alt}>
+    </div>
+    <div class="content">
+      <div class="name">
+        <h3>
+          {item.name}
+        </h3>
+        {#if item.position && item.position !== ''}
+          <div class="position">
+            {item.position}
+          </div>
+        {/if}
+      </div>
+      {#if item.phone && item.phone !== ''}
+        <div class="phone">
+          <h4>
+            Номер телефона для связи:
+          </h4>
+          <div class="phone">
+            {item.phone}
+          </div>
+        </div>
+      {/if}
+      {#if item.email && item.email !== ''}
+        <div class="email">
+          <h4>
+            Адрес электронной почты для связи:
+          </h4>
+          <div class="email">
+            {item.email}
+          </div>
+        </div>
+      {/if}
+    </div>
+  </article>
+{/if}
+
+<style>
+  article {
+    border: 1px solid #363433;
+  }
+  img {
+    width: 100%;
+  }
+  .content {
+    font-size: 16px;
+    line-height: 19px;
+    padding: 20px 16px 24px 16px;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 19px;
+  }
+  .phone > *,
+  .email > * {
+    display: inline-block;
+  }
+</style>
