98a814c98a0d81de162afb8091b86e50e3b24c0c
Den Kochetkov <d@zdp.su>
2021-09-05 15:54:28 +0300

    ContactsHero mobile layout done

---
+++ web-service/src/lib/ContactsHero/index.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let contactsHeroData;
+</script>
+
+{#if contactsHeroData}
+  <section>
+    <div class="container">
+      <img src={contactsHeroData.bg.file.url} alt={contactsHeroData.bg.alt}>
+      <div class="overlay" style="background: {contactsHeroData.bg.overlay};"></div>
+      <h2>
+        {contactsHeroData.header}
+      </h2>
+    </div>
+  </section>
+{/if}
+
+<style>
+  section {
+    position: relative;
+    z-index: 1;
+    padding: 105px 0;
+    color: #fff;
+  }
+  img {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    z-index: -2;
+  }
+  .overlay {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+  }
+  h2 {
+    font-size: 40px;
+    line-height: 70px;
+  }
+</style>
