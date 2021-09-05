d4440bbdfbc2c4ab1a224d3aefaf736f7bd3cb66
Den Kochetkov <d@zdp.su>
2021-09-05 21:38:13 +0300

    Purchasing component done

--- web-service/src/lib/Employees/ItemCard.svelte
+++ web-service/src/lib/Employees/ItemCard.svelte
@@ -11,31 +11,41 @@
       </h2>
     {/if}
     <article>
-      <img src={item.photo.file.url} alt={item.photo.alt}>
-      <h3>
-        {item.name}
-      </h3>
-      {#if item.position}
-        <div class="position">
-          {item.position}
+      <div class="photo">
+        <img src={item.photo.file.url} alt={item.photo.alt}>
+      </div>
+      <div class="content">
+        <div class="name">
+          <h3>
+            {item.name}
+          </h3>
+          {#if item.position}
+            <div class="position">
+              {item.position}
+            </div>
+          {/if}
         </div>
-      {/if}
-      {#if item.phone && item.phone !== ''}
-        <h4 class="phone-header">
-          Номер телефона для связи:
-        </h4>
-        <div class="phone">
-          {item.phone}
-        </div>
-      {/if}
-      {#if item.email && item.email !== ''}
-        <h4 class="email-header">
-          Адрес электронной почты для связи:
-        </h4>
-        <div class="email">
-          {item.email}
-        </div>
-      {/if}
+        {#if item.phone && item.phone !== ''}
+          <div class="phone">
+            <h4 class="phone-header">
+              Номер телефона для связи:
+            </h4>
+            <div class="phone">
+              {item.phone}
+            </div>
+          </div>
+        {/if}
+        {#if item.email && item.email !== ''}
+          <div class="email">
+            <h4 class="email-header">
+              Адрес электронной почты для связи:
+            </h4>
+            <div class="email">
+              {item.email}
+            </div>
+          </div>
+        {/if}
+      </div>
     </article>
   </div>
 {/if}
@@ -52,23 +62,99 @@
   h2 {
     margin-bottom: 30px;
   }
+  .photo {
+    margin-bottom: 20px;
+  }
   img {
     width: 100%;
-    margin-bottom: 20px;
   }
   h3,
   h4 {
     font-size: 16px;
     line-height: 19px;
+  }
+  .content {
     padding: 0 20px 0 15px;
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 19px;
+    align-content: center;
+    align-items: center;
   }
-  .position,
-  .phone,
-  .email {
-    padding: 0 20px 0 15px;
+  @media (min-width: 576px) {
+    article {
+      grid-template-columns: 5fr 9fr;
+      column-gap: 35px;
+      padding-bottom: 0;
+      min-height: 223px;
+    }
+    .content {
+      padding: 33px 30px 23px 0;
+      align-content: center;
+    }
+    .photo {
+      margin-bottom: 0;
+      align-self: stretch;
+      height: 100%;
+    }
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      object-position: center;
+    }
+    .wrapper:nth-of-type(even) article {
+      grid-template-columns: 9fr 5fr;
+    }
+    .wrapper:nth-of-type(even) article {
+      column-gap: 35px;
+    }
+    .wrapper:nth-of-type(even) .photo {
+      grid-column: 2 / 3;
+    }
+    .wrapper:nth-of-type(even) .content {
+      grid-row: 1 / 2;
+      padding: 33px 0 23px 30px;
+      text-align: right;
+    }
   }
-  .phone-header,
-  .email-header {
-    margin-top: 19px;
+
+  @media (min-width: 768px) {
+    h2 {
+      margin-bottom: 40px;
+    }
+    .content {
+      padding: 33px 68px 23px 0;
+      align-content: center;
+    }
+    .wrapper:nth-of-type(even) .content {
+      grid-row: 1 / 2;
+      padding: 33px 63px 23px 30px;
+      text-align: right;
+    }
   }
+
+  @media (min-width: 992px) {
+    h2 {
+      margin-bottom: 60px;
+    }
+    article {
+      min-height: 280px;
+    }
+    article,
+    .wrapper:nth-of-type(even) article {
+      height: 100%;
+      grid-template-columns: 1fr 1fr;
+      column-gap: 20px;
+    }
+    .content {
+      padding: 33px 20px 23px 0;
+      align-content: center;
+    }
+    .wrapper:nth-of-type(even) .content {
+      padding: 33px 0 23px 20px;
+      text-align: left;
+    }
+  }
+
 </style>
--- web-service/src/lib/Employees/Purchasing.svelte
+++ web-service/src/lib/Employees/Purchasing.svelte
@@ -31,4 +31,29 @@
   h2 {
     margin-bottom: 10px;
   }
+
+  @media (min-width: 768px) {
+    section {
+      padding: 80px 0;
+    }
+    .container {
+      row-gap: 30px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    section {
+      padding: 100px 0;
+    }
+    .container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      row-gap: 60px;
+    }
+    h2 {
+      grid-column: span 2;
+      margin-bottom: 0;
+    }
+  }
+
 </style>
