326415dc6bfec2b9d295e14a78204af7bc00bba1
Den Kochetkov <d@zdp.su>
2021-09-05 21:41:53 +0300

    BookkeepingHr component done

--- web-service/src/lib/Employees/BookkeepingHr.svelte
+++ web-service/src/lib/Employees/BookkeepingHr.svelte
@@ -1,12 +1,19 @@
 <script>
+  import ItemCard from './ItemCard.svelte';
+
   export let bkData;
   export let hrData;
 </script>
 
-{#if bkData && hrData}
+{#if bkData || hrData}
   <section>
     <div class="container">
-      Bookkeeping & Hr
+      {#each bkData.items as item}
+        <ItemCard bind:item bind:header={bkData.header} />
+      {/each}
+      {#each hrData.items as item}
+        <ItemCard bind:item bind:header={hrData.header} />
+      {/each}
     </div>
   </section>
 {/if}
@@ -17,4 +24,37 @@
     background: #363433;
     color: #fff;
   }
+  .container {
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 20px;
+  }
+  h2 {
+    margin-bottom: 10px;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      padding: 80px 0;
+    }
+    .container {
+      row-gap: 30px;
+    }
+  }
+
+  @media (min-width: 992px) {
+    section {
+      padding: 100px 0;
+    }
+    .container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      row-gap: 60px;
+    }
+    h2 {
+      grid-column: span 2;
+      margin-bottom: 0;
+    }
+  }
+
 </style>
