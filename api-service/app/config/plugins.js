a1e78d391319cbba2b55d899ca9780958b546330
Den Kochetkov <d@zdp.su>
2021-09-10 18:23:25 +0300

    sendgrid email provider added to API

---
+++ api-service/app/config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  email: {
+    provider: 'sendgrid',
+    providerOptions: {
+      apiKey: env('SENDGRID_API_KEY'),
+    },
+    settings: {
+      defaultFrom: 'robot@asknotbad.com.com',
+      defaultReplyTo: 'd@asknotbad.com',
+      testAddress: 'd@asknotbad.com',
+    },
+  },
+});
