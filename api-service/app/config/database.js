ad6ab51f99ca3a23e00c6a417f419da1234ed0b6
Den Kochetkov <d@zdp.su>
2021-11-18 12:38:21 +0300

    traefik service added

--- api-service/app/config/database.js
+++ api-service/app/config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'postgres'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'promresource'),
-        username: env('DATABASE_USERNAME', 'promresource'),
-        password: env('DATABASE_PASSWORD', 'password'),
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
